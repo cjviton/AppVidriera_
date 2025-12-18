f547f7fa5077fe2eeb52c80ae77ae47fc139d261
= <=>
2025-12-18 13:21:51 +0100

    no se

--- ApiConexSql/obj/Debug/net8.0/ApiConexSql.AssemblyInfo.cs
+++ ApiConexSql/obj/Debug/net8.0/ApiConexSql.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiConexSql")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78bc6b74ceeaaa9e5f8c47d8ead3597688e3eb6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70955bce1567830fedb0a32eee8259ab76410ae7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiConexSql")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiConexSql")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
